311cb008c66e568ad9593fd47d75b1b1ce2c61e3
MartinRep <repickym@gmail.com>
2019-06-14 00:35:54 +0100

    Install script tweak

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo 'Installing required python packages...'
+pip install -r requirements.txt
+echo 'Creating executable script...'
 touch github
 echo '#!/usr/bin/env python' > github
 cat github.py >> github
 chmod +x github
+echo 'Copying script to local bin folder'
 sudo cp ./github /usr/local/bin
 sudo chmod +x /usr/local/bin/github
