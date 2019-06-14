1c733c418028e5ac14f43af7174052b78030611a
MartinRep <repickym@gmail.com>
2019-06-14 01:54:13 +0100

    Few logical fixxes

--- github.py
+++ github.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 import subprocess
 
 # GitHub access token
-GH_tokken = "Your own GitHub Token" # Get your own from https://github.com/settings/tokens
+GH_tokken = "4470ee3a1714f9f010316e70d51ee1a7d37f438b" # Get your own from https://github.com/settings/tokens
 
 # Your favourite text editor. Default is Visual Studio code
 editor = "code"
@@ -14,14 +14,13 @@ editor = "code"
 projects_dir =  expanduser("~") + "/Projects/"
 project_path = projects_dir
 
-
 def create_dir():
     if not os.path.isdir(projects_dir):
         print("Creating Projects folder.")
         os.makedirs(projects_dir)
     if not os.path.isdir(project_path):        
         os.makedirs(project_path)
-    print("Folder {} created.".format(project_path))
+        print("Folder {} created.".format(project_path))
  
 def init_github_repo(project_name):
     myGH = Github(GH_tokken) 
@@ -29,18 +28,24 @@ def init_github_repo(project_name):
     print("Checking if project repository alredy exists...")
     for repo in user.get_repos():
         if project_name == repo.name:
-            print("Repository {} alredy exist on Github. Just cloning the repo...")
-            if os.path.isdir(project_path):
+            print("Repository {} alredy exist on Github. Just cloning the repo...".format(project_name))
+            if os.path.isdir(project_path + "/.git"):
                 print("Repository folder already exist, Repository Pull initialized..")
                 os.chdir(projects_dir)
                 git.Repo(project_name).remotes.origin.pull()
                 return
+            git.Repo.clone_from(repo.html_url, project_path)            
+            return
     print("Repository not found, creating a new one.")
     repo = user.create_repo(project_name)
     git.Repo.clone_from(repo.html_url, project_path)
 
 def main():
     global project_path
+    #Prints out small help info
+    if sys.argv[1] == "--help" or sys.argv[1] == "-H":
+        print("Github repository tool.\n Create or Clone GitHub project and start editor.\n github <project name>")
+        return
     if len(sys.argv) is 1:
         project_name = "project-" + str(int(time.time()))
     else:
@@ -48,10 +53,8 @@ def main():
     project_path = projects_dir + project_name
     # Creates Project folder
     create_dir()
-
+    # Deals with github repos
     init_github_repo(project_name)
-    # Creates new repository on Github.com and clones it into the folder
-    # git.Repo.clone_from(create_github_repo(project_name), project)
     # Runs Visual Studio Code in the new Project folder
     print("Starting editor...")
     subprocess.run([editor, project_path])
